b35739112d5e4f62dbd94553b1469e261c595513
shams git <shamskhurasani481@gmail.com>
2021-06-23 01:16:29 +0200

    Adding includes folder, replace 404.php to the includesfolder, modifying redirection

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
 <?php
-// phpinfo();
-echo "<pre>";
-// var_dump($_SERVER["HTTP_ACCEPT"]);
-// var_dump(getenv('PHP_ENV'), $_SERVER, $_REQUEST);
-// var_dump($_SERVER['REQUEST_METHOD']);
-// echo '<hr>';
-// var_dump($_SERVER['REQUEST_URI']);
-$requestUrl = $_SERVER['REQUEST_URL'] ?? '/';
+// phpinfo();// show all php informations
+$requestUrl = $_SERVER['REQUEST_URL'] ?? '/';// The two quuestion mark means tha if the left side is NULL or.. .
+// ...udefined then use the things on the right side.
 $requestMethode = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 // echo "<br>Request url is: ".$requestUrl;
 // echo "Request method is: ".$requestMethode;
 echo "<h3>Home page</h3>";
 echo "<h2>Welcome to whoosh site</h2>";
-if($requestMethode === 'GET' && $requestUrl ==='/'){
-    echo "Hello, your url addresse is correct";
+
+// Why using and but not && ?
+// There are not deffernce between AND and && but using AND is good.
+// Why using === but not == ?
+// 
+if($requestMethode === 'GET' and $requestUrl ==='/'){
+// print creats a group of html elements
+print '<!DOCTYPE html>
+        <html lang=\"en\">
+            <head>
+                <meta charset=\"UTF-8\">
+                <title>Document</title>
+            </head>
+            <body>
+                <h4>Hello, your url addresse is correct</h4>
+            </body>
+        </html>';
 } else {
     // require_once('404.php');
-    header('Location: 404.php');
-    exit();
+    // header('Location: includes/404.php');
+    include(__DIR__.'/includes/404.php');
     // header('Location: http://www.example.com/');
+    exit();
 }
-
-echo "</pre>";
-
-
 
